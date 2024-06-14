5a0d8165b854a1b7f12d02adc298fb9556d7e0de
eric2302 <ericgc95@gmail.com>
2024-06-14 18:57:07 -0400

    [ENH] added gene null function and subcortical reordering

--- utils.py
+++ utils.py
@@ -1,7 +1,195 @@
 import itertools
+import random
 import numpy as np
 import pandas as pd
-import scipy
+from scipy.sparse.linalg import expm
+from scipy.stats import ks_2samp
+from joblib import Parallel, delayed
+
+def morans_i(dist, y, normalize=False, local=False, invert_dist=True):
+    """
+    Calculates Moran's I from distance matrix `dist` and brain map `y`.
+    Taken from Markello et al 2020 (Neuroimage).
+
+    Parameters
+    ----------
+    dist : (N, N) array_like
+        Distance matrix between `N` regions / vertices / voxels / whatever
+    y : (N,) array_like
+        Brain map variable of interest
+    normalize : bool, optional
+        Whether to normalize rows of distance matrix prior to calculation.
+        Default: False
+    local : bool, optional
+        Whether to calculate local Moran's I instead of global. Default: False
+    invert_dist : bool, optional
+        Whether to invert the distance matrix to generate a weight matrix.
+        Default: True
+
+    Returns
+    -------
+    i : float
+        Moran's I, measure of spatial autocorrelation
+    """
+    # Ensure dist and y are numpy arrays
+    dist = np.array(dist)
+    y = np.array(y)
+
+    # convert distance matrix to weights
+    if invert_dist:
+        with np.errstate(divide='ignore'):
+            dist = 1 / dist
+    np.fill_diagonal(dist, 0)
+
+    # normalize rows, if desired
+    if normalize:
+        dist /= dist.sum(axis=-1, keepdims=True)
+
+    # calculate Moran's I
+    z = y - y.mean()
+    if local:
+        with np.errstate(all='ignore'):
+            z /= y.std()
+
+    zl = np.squeeze(dist @ z[:, np.newaxis])
+    den = (z * z).sum()
+
+    if local:
+        return (len(y) - 1) * z * zl / den
+
+    return len(y) / dist.sum() * (z * zl).sum() / den 
+
+
+def get_gene_null(gene_set, gene_set_mi, non_overlapping_set, distance, null_set_size=100, seed=1234):
+    """
+    Creates a null set of gene expression data by randomly selecting genes from a non-overlapping set
+    and matching the spatial autocorrelation and value distribution of the original gene set.
+    Moran's I is used to match the spatial autocorrelation. Kolmogorov-Smirnov test is used to match 
+    the value distribution.
+    
+    Parameters
+    ----------
+    gene_set : pandas.DataFrame
+        Gene expression data to be matched
+    gene_set_mi : numpy.ndarray
+        Spatial autocorrelation of gene expression data, derived from Moran's I. Should be equal to the
+        number of genes in gene_set
+    non_overlapping_set : pandas.DataFrame
+        Gene expression data to be used for null set sampling
+    distance : numpy.ndarray
+        Distance matrix for spatial autocorrelation
+    null_set_size : int
+        Number of genes to sample for null set
+    seed : int
+        Random seed for reproducibility
+
+    Returns
+    -------
+    null : numpy.ndarray
+        Array containing gene expression data matching the spatial autocorrelation and value distribution
+        of the original gene set
+    """
+    # sample genes
+    random_genes = non_overlapping_set.sample(n=null_set_size, axis=1, random_state=seed)
+    # get the spatial autocorrelation of each gene in the random gene set
+    random_mi_values = []
+    random_ks_values = []
+    for _, random_gene in random_genes.items():
+        random_mi_values.append(morans_i(distance, random_gene.values))
+        random_ks_values.append([ks_2samp(gene, random_gene.values).statistic for _, gene in gene_set.items()])
+    random_mi_values = np.array(random_mi_values)
+    random_ks_values = np.array(random_ks_values)
+
+    # for each gene in gene_set, calculate which moran_i is most similar among the random genes
+    comparison_mi = [np.abs(gene_set_mi - random_mi_values[i]) for i in range(null_set_size)]
+    rank_mi = np.argsort(comparison_mi, axis=0)
+
+    # get the value distribution of each gene in the gene set
+    rank_ks = np.argsort(random_ks_values, axis=0)  
+
+    # select the gene with the highest average rank
+    rank = (rank_mi + rank_ks) / 2
+    indices = np.argmin(rank, axis=0)
+    selected_genes = random_genes.iloc[:, indices]
+    null = selected_genes.values
+    return null
+
+
+def gene_null_set(gene_set, non_overlapping_set, distance, null_set_size=100, n_permutations=1000, 
+                  n_jobs=1, seed=1234):
+    """
+    Creates a null set of gene expression data by randomly selecting genes from a non-overlapping set
+    and matching the spatial autocorrelation and value distribution of the original gene set.
+    Moran's I is used to match the spatial autocorrelation. Kolmogorov-Smirnov test is used to match 
+    the value distribution.
+    
+    Parameters
+    ----------
+    gene_set : pandas.DataFrame
+        Gene expression data to be matched
+    non_overlapping_set : pandas.DataFrame
+        Gene expression data to be used for null set
+    distance : numpy.ndarray
+        Distance matrix for spatial autocorrelation
+    n_permutations : int
+        Number of permutations to create
+    seed : int
+        Random seed for reproducibility
+
+    Returns
+    -------
+    nulls : list
+        List of numpy.ndarray containing null gene expression data
+    """
+    rng = np.random.default_rng(seed)
+    # seeds = [rng.integers(0, 2**32-1) for _ in range(n_permutations)]
+
+    # get the spatial autocorrelation of each gene in the gene set
+    moran_i = []
+    for gene in gene_set.columns:
+        moran_i.append(morans_i(distance, gene_set[gene].values))
+    moran_i = np.array(moran_i)
+
+    # # make sure to have as many genes as regions
+    # null_set_size = max(n_regions, null_set_size)
+
+    # parallelize using joblib
+    null_set = Parallel(n_jobs=n_jobs,)(delayed(get_gene_null)(gene_set, moran_i, non_overlapping_set, distance, 
+                                                              null_set_size=null_set_size, seed=i)
+                                      for i in range(n_permutations))
+
+
+    # # sample genes from non_overlapping_set
+    # null_set = []
+    # for _ in range(n_permutations):
+    #     # sample genes
+    #     random_genes = non_overlapping_set.sample(n=null_set_size, axis=1, random_state=rng.integers(0, 2**32-1))
+    #     # get the spatial autocorrelation of each gene in the random gene set
+    #     random_mi_values = []
+    #     random_ks_values = []
+    #     for _, random_gene in random_genes.items():
+    #         random_mi_values.append(morans_i(distance, random_gene.values))
+    #         random_ks_values.append([ks_2samp(gene, random_gene.values).statistic for _, gene in gene_set.items()])
+    #     random_mi_values = np.array(random_mi_values)
+    #     random_ks_values = np.array(random_ks_values)
+
+    #     # for each gene in gene_set, calculate which moran_i is most similar among the random genes
+    #     comparison_mi = [np.abs(moran_i - random_mi_values[i]) for i in range(null_set_size)]
+    #     rank_mi = np.argsort(comparison_mi, axis=0)
+
+    #     # get the value distribution of each gene in the gene set
+    #     rank_ks = np.argsort(random_ks_values, axis=0)  
+
+    #     # select the gene with the highest average rank
+    #     rank = (rank_mi + rank_ks) / 2
+    #     indices = np.argmin(rank, axis=0)
+    #     selected_genes = random_genes.iloc[:, indices]
+    #     null_set.append(selected_genes.values)
+        
+    return null_set
+    
+
+
 
 def navigation_wu(nav_dist_mat, sc_mat, show_progress=True):
     from tqdm import tqdm
@@ -63,8 +251,12 @@ def search_information(W, L, has_memory=False):
         flag_triu = True
     else:
         flag_triu = False
-
-    T = np.linalg.solve(np.diag(np.sum(W, axis=1)), W)
+    try:
+        T = np.linalg.solve(np.diag(np.sum(W, axis=1)), W)
+    except np.linalg.LinAlgError:
+        # singular matrix! solve for x using pseudo-inverse
+        T = np.linalg.pinv(np.diag(np.sum(W, axis=1))) @ W
+        
     _, hops, Pmat = bc.distance.distance_wei_floyd(L, transform=None)
 
     SI = np.zeros((N, N))
@@ -155,6 +347,7 @@ def communicability_wei(adjacency):
 
     # negative square root of nodal degrees
     row_sum = adjacency.sum(1)
+    row_sum[row_sum == 0] = 1e-10 # add small value to avoid division by zero
     neg_sqrt = np.power(row_sum, -0.5)
     square_sqrt = np.diag(neg_sqrt)
 
@@ -162,7 +355,7 @@ def communicability_wei(adjacency):
     for_expm = square_sqrt @ adjacency @ square_sqrt
 
     # calculate matrix exponential of normalized matrix
-    cmc = scipy.sparse.linalg.expm(for_expm)
+    cmc = expm(for_expm)
     cmc[np.diag_indices_from(cmc)] = 0
 
     return cmc
@@ -192,3 +385,64 @@ def index_structure(df, structure='CTX-SBCTX'):
     else:
         raise ValueError('Invalid structure. Must be CTX, SBCTX, or CTX-SBCTX')
     return indexed_df
+
+
+def reorder_subcortex(gene_df, type='freesurfer', region_info=None):
+    """
+    Reorders the gene expression data for subcortex regions to match the freesurfer or enigma atlas.
+    Parameters
+    ----------
+    gene_df : pandas.DataFrame
+        Gene expression data to be reordered
+    type : str
+        Type of atlas to reorder to. Can be 'freesurfer' or 'enigma'
+    region_info : pandas.DataFrame
+        Region information dataframe to reorder to freesurfer atlas
+    Returns
+    -------
+    gene_df : pandas.DataFrame
+        Reordered gene expression data
+    """
+
+    # is it freesurfer or enigma?
+    if type not in ['freesurfer', 'enigma']:
+        raise ValueError("Invalid type. Must be 'freesurfer' or 'enigma'")
+    
+    ctx_genes = index_structure(gene_df, structure='CTX')
+    sbctx_genes = index_structure(gene_df, structure='CTX-SBCTX')
+
+    regions = sbctx_genes.index.str.split('-').str[0]
+    hemi = sbctx_genes.index.str.split('-').str[-1]
+
+    name_map = {'HIP': 'Hippocampus' if type == 'freesurfer' else 'hippocampus',
+                'THA': 'Thalamus-Proper' if type == 'freesurfer' else 'thalamus',
+                'mAMY': 'Amygdala' if type == 'freesurfer' else 'amygdala',
+                'lAMY': 'Amygdala' if type == 'freesurfer' else 'amygdala',
+                'PUT': 'Putamen' if type == 'freesurfer' else 'putamen',
+                'aGP': 'Pallidum' if type == 'freesurfer' else 'pallidum',
+                'pGP': 'Pallidum' if type == 'freesurfer' else 'pallidum',
+                'CAU': 'Caudate' if type == 'freesurfer' else 'caudate',
+                'NAc': 'Accumbens-area' if type == 'freesurfer' else 'accumbens',}
+
+    hemi_map = {'lh': 'L' if type == 'freesurfer' else 'left',
+                'rh': 'R' if type == 'freesurfer' else 'right'}
+
+    # map regions/ hemi to names
+    regions = regions.map(name_map)
+    hemi = hemi.map(hemi_map)
+
+    # join the two and rename index
+    sbctx_genes.index = hemi + '-' + regions
+
+    # average same regions
+    sbctx_genes = sbctx_genes.groupby(sbctx_genes.index).mean()
+    if type == 'freesurfer':
+        if region_info is None:
+            raise ValueError("'region_info' must be provided for freesurfer reordering")
+        # order according to region_info
+        filtered_labels = [label for label in region_info['labels'] if label in sbctx_genes.index]
+        sbctx_genes = sbctx_genes.loc[filtered_labels]
+
+    # concatenate subcortex and cortex
+    gene_df = pd.concat([sbctx_genes, ctx_genes])
+    return gene_df
