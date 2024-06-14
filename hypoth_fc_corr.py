1c5135fc94ea33993f05e3f75139f5db7745f1e1
eric2302 <ericgc95@gmail.com>
2024-06-14 18:55:39 -0400

    [FIX] fixed bugs and cleaned up

--- hypoth_fc_corr.py
+++ hypoth_fc_corr.py
@@ -4,52 +4,77 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import spearmanr
+from sympy import plot
 from utils import index_structure, reorder_subcortex, gene_null_set
 from plot_utils import divergent_green_orange
 from netneurotools import plotting, modularity
+from neuromaps.stats import compare_images
 
 # %% LOAD BRAINSTEM FUNCTIONAL CONNCETIVITY DATA
-# open brainstem data under data/brainstemfc_mean_corrcoeff_full.npy
-bs = np.load('data/brainstemfc_mean_corrcoeff_full.npy')
-
-# load region_info
+hth_fc = pd.read_csv('data/hth_fc_Schaefer400_Fischl14.csv', index_col=0).values#.flatten()
+# region info from Hansen et al. 2023
 region_info = pd.read_csv('data/parcellations/Schaefer2018_400_region_info_brainstem_subcortex.csv', index_col=0)
 
-# find hypothalamus stored as HTH
-hth_idx = region_info[region_info['labels'] == 'HTH'].index[0]
-
-# find cerebellum index and drop from region_info
-cerebellum_idx = region_info[region_info['labels'].str.contains('Cereb-Ctx')].index
-region_info = region_info.drop(cerebellum_idx)
-
-# find subcortex and cortex index and put them together
-cortex_idx = region_info[region_info['structure'] == 'cortex'].index.values
-subcortex_idx = region_info[region_info['structure'] == 'subcortex'].index.values
-idx = np.concatenate([subcortex_idx, cortex_idx])
-
-# store functional connectivity between hypothalamus and subcortex/cortex
-hth_fc = bs[hth_idx, idx]
-
-
-# %% GET EXPRESSION FROM MATCHING REGIONS
+# %% GET EXPRESSION FROM MATCHING REGIONS AND CREATE NULL GENE SET
 receptor_genes = pd.read_csv('data/receptor_gene_expression_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
-receptor_genes = reorder_subcortex(receptor_genes, type='freesurfer', region_info=region_info)
+receptor_genes = index_structure(receptor_genes, structure='CTX-SBCTX')
 
-# %% 
-# correlation between hypothalamus functional connectivity and gene expression
-fc_gene_corr = np.array([spearmanr(hth_fc, receptor_genes[gene])[0] for gene in receptor_genes.columns])
+# load all genes from abagen
+all_genes = pd.read_csv('data/abagen_gene_expression_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
+all_genes = index_structure(all_genes, structure='CTX-SBCTX')
 
-# plot correlation values in descending order
-df = pd.DataFrame(np.vstack([receptor_genes.columns, fc_gene_corr]).T, columns=['Gene', 'HTH_corr'])
-df['HTH_corr'] = df['HTH_corr'].astype(float)
+# load list of peptide genes
+peptide_list = pd.read_csv('data/gene_list.csv')['Gene']
+
+# get non-peptide genes
+non_peptide_genes = all_genes.T[~all_genes.columns.isin(peptide_list)].T
+
+# need distance matrix for null function
+distance = np.load('data/template_parc-Schaefer400_TianS4_desc-distance.npy')
+
+# generate nulls
+nulls = gene_null_set(receptor_genes, non_peptide_genes, distance, n_permutations=10000, 
+                      n_jobs=32, seed=1234)
+
+nulls = np.array(nulls)
+np.save('data/gene_null_sets_Schaefer400_TianS4.npy', nulls)
+
+# %%
+# reorder null genes to match HTH FC
+receptor_genes = pd.read_csv('data/receptor_gene_expression_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
+null_ro = []
+for null in nulls:
+    # add last row of NaNs for HTH
+    null = np.vstack([null, np.nan*np.ones(null.shape[1])])
+    null_df = pd.DataFrame(null, index=receptor_genes.index)
+    null_ro.append(reorder_subcortex(null_df, type='freesurfer', region_info=region_info).values)
+null_ro = np.array(null_ro)
+
+# %%
+# reorder receptor genes to match HTH FC
+receptor_genes_rs = reorder_subcortex(receptor_genes, type='freesurfer', region_info=region_info).values
+
+# iterate through genes and compare to the hth_fc distribution
+results = np.array([compare_images(receptor_genes_rs[:, i], hth_fc, nulls=null_ro[:,:,i].T, metric='spearmanr') 
+                    for i in range(null_ro.shape[2])])
+
+fc_gene_corr, p_values = results[:, 0], results[:, 1]
+
+# %%
+# build dataframe for results
+df = pd.DataFrame({'Gene': receptor_genes.columns, 'HTH_corr': fc_gene_corr, 'p': p_values})
 df['Sign'] = df['HTH_corr'] > 0
-df = df.sort_values('HTH_corr', ascending=False)
-df['Gene'] = df['Gene'].str.replace(' ', '\n')
 
 fig, ax = plt.subplots(figsize=(5,6), dpi=200)
 palette = divergent_green_orange(n_colors=9, return_palette=True)
 colors = [palette[1], palette[-2]]
-sns.barplot(data=df, x='HTH_corr', y='Gene', hue='Sign', dodge=False, ax=ax, palette=colors)
+plot_df = df.sort_values('HTH_corr', ascending=False).copy()
+sns.barplot(data=plot_df, x='HTH_corr', y='Gene', hue='Sign', dodge=False, ax=ax, palette=colors)
+# add asterisks on the side if p-value < 0.05
+for i, p in enumerate(plot_df['p']):
+    if p < 0.05:
+        ax.text(plot_df['HTH_corr'].max()+0.1, i+0.6, '*', fontsize=12, color='black')
+
 ax.set(xlabel=r'Similarity ($\rho$)', ylabel='Gene', title='Aligment with FC$_{Hypothalamus}$')
 ax.legend_.remove()
 fig.set_tight_layout(True)
@@ -77,19 +102,3 @@ colors = [palette[1], palette[4], palette[7]]
 sns.violinplot(data=df, x='ci', y='HTH_corr', ax=ax, palette=colors)
 plt.ylabel('Similarity to FC$_{Hypothalamus}$')
 plt.xlabel('Module')
-
-
-# %% CREATE NULL GENE SET
-
-# load all genes from abagen
-all_genes = pd.read_csv('data/abagen_gene_expression_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
-all_genes = index_structure(all_genes, structure='CTX-SBCTX')
-# load list of peptide genes
-peptide_list = pd.read_csv('data/gene_list.csv')['Gene']
-# get non-peptide genes
-non_peptide_genes = all_genes.T[~all_genes.columns.isin(peptide_list)].T
-# non_peptide_genes = reorder_subcortex(non_peptide_genes, type='freesurfer', region_info=region_info)
-
-# load distance matrix
-distance = np.load('data/template_parc-Schaefer400_TianS4_desc-distance.npy')
-nulls = gene_null_set(receptor_genes, non_peptide_genes, distance, n_permutations=1000, seed=1234)
