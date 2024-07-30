5da88a52a3a8fbc5cc203cb7f94fa27b6d8b0225
eric2302 <ericgc95@gmail.com>
2024-07-30 13:27:37 -0400

    [ENH] added stratification by network

--- 1-2_cortex_hypothalamus_comparison.py
+++ 1-2_cortex_hypothalamus_comparison.py
@@ -4,40 +4,62 @@ import abagen
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from utils import index_structure
 
 # %%
 # load the genes
-all_genes = pd.read_csv('./data/abagen_genes_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
+all_genes = pd.read_csv('./data/abagen_gene_expression_Schaefer2018_400_7N_Tian_Subcortex_S4.csv', index_col=0)
 
 # load atlas info
-atlas_info = pd.read_csv('./data/parcellations/Schaefer2018_400_7N_Tian_Subcortex_S4_LUT.csv')
+atlas_info = pd.read_csv('./data/parcellations/Schaefer2018_400_7N_Tian_Subcortex_S4_LUT.csv', index_col=0)
+atlas_info['network'] = atlas_info['name'].str.split('_').str[0].str.split('-').str[0]
+atlas_info['network'] = atlas_info['network'].str.replace('lAMY', 'AMY').str.replace('mAMY', 'AMY')
+atlas_info['network'] = atlas_info['network'].str.replace('aGP', 'GP').str.replace('pGP', 'GP')
+
+# create name to network mapper
+mapper = dict(zip(atlas_info['name'], atlas_info['network']))
 
 # load list of relevant genes
 gene_list = pd.read_csv('data/gene_list.csv')
 
-# divide into subcortex and cortex
-subcortex = atlas_info[atlas_info['structure'] == 'subcortex'].iloc[:-1] # remove hypothalamus
-cortex = atlas_info[atlas_info['structure'] == 'cortex']
+receptor_names = pd.read_csv('data/receptor_qc.csv', index_col=0).index
+precursor_names = pd.read_csv('data/precursor_qc.csv', index_col=0).index
 
-# find overlapping genes in genes columns and gene_list
-overlapping_genes = gene_list[gene_list['Gene'].isin(all_genes.columns)]['Gene']
+# keep only genes that are in all_genes
+receptor_names =  receptor_names[receptor_names.isin(all_genes.columns)]
+precursor_names = precursor_names[precursor_names.isin(all_genes.columns)]
 
 # select overlapping genes from genes and gene_list
-genes = all_genes[overlapping_genes]
-gene_list = gene_list[gene_list['Gene'].isin(overlapping_genes)]
+receptor_genes = all_genes[receptor_names]
+precursor_genes = all_genes[precursor_names]
 
-receptor_labels = gene_list[gene_list['Description'].str.contains('receptor')]
-receptor_labels = receptor_labels.append({'Gene': 'SORT1'}, ignore_index=True)
-peptide_labels = gene_list[~gene_list['Gene'].isin(receptor_labels['Gene'])]
-receptor_genes = all_genes[receptor_labels['Gene']]
-peptide_genes = all_genes[peptide_labels['Gene']]
+# split precursor into cortex, subcortex and hth. rename their index as ctx, sbctx and hth respectively
+# precursor_ctx = index_structure(precursor_genes, structure='CTX').rename(index=lambda x: 'CTX')
+# precursor_sbctx = index_structure(precursor_genes, structure='SBCTX').rename(index=lambda x: 'SBCTX')
+
+# map the index to the network
+precursor_ctx = index_structure(precursor_genes, structure='CTX').rename(index=mapper)
+precursor_sbctx = index_structure(precursor_genes, structure='SBCTX').rename(index=mapper)
+precursor_hth = index_structure(precursor_genes, structure='HTH').to_frame().T  # type:ignore
+
+# merge into one tall dataframe
+precursor_genes = pd.concat((precursor_ctx, precursor_sbctx, precursor_hth)).reset_index() # type:ignore
+precursor_genes = precursor_genes.melt(id_vars='index', var_name='gene', value_name='expression')
+
+# plot boxplot of precursor gene expression in cortex, subcortex and hypothalamus
+fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+sns.boxplot(data=precursor_genes, x='index', y='expression', ax=ax)
+ax.set_title('Peptide precursor gene expression')
+ax.set_ylabel('Gene expression')
+ax.set_xlabel('Structure')
+plt.xticks(rotation=45)
+
+if savefig:
+    plt.savefig('figs/precursor_gene_expression.pdf')
+
+# ax.set_xticklabels(['Cortex', 'Subcortex', 'Hypothalamus'])
 
 # %%
-# split genes dataframe by cortex and subcortex
-subcortex_genes = peptide_genes.loc[subcortex['name']]
-cortex_genes = peptide_genes.loc[cortex['name']]
-hth_genes = peptide_genes.loc['HTH']
-
 # compare subcortex and cortex boxplots with hypothalamus as a dot
 fig, ax = plt.subplots(2, 1, figsize=(20, 10))
 
@@ -49,7 +71,7 @@ for i in range(len(subcortex_genes.columns)):
     ax[0].scatter(i, hth_genes[subcortex_genes.columns[i]], color='red', label='Hypothalamus')
 for i in range(len(cortex_genes.columns)):
     ax[1].scatter(i, hth_genes[cortex_genes.columns[i]], color='red', label='Hypothalamus')
-ax[0].set_title('Prepropetide gene expression')
+
 
 ax[0].set_ylabel('Gene expression')
 ax[1].set_ylabel('Gene expression')
